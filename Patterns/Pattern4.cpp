04cd8e2ac3745d7b3acda92903a121ff29432098
Divyam Verma <60467333+pokeychip19946@users.noreply.github.com>
2022-01-13 00:30:44 +0530

    Create Pattern4.cpp

---
+++ Patterns/Pattern4.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifndef ONLINE_JUDGE
+#define freopen freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+#else
+#define freopen 
+#endif
+
+int main()
+{
+  freopen;
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+      for(int j=0; j<n; j++)
+      {
+        if(j>=i) cout<<"*";
+        else cout<<" ";
+      }
+      cout<<"\n";
+    }
+  }
+
+  return 0;
+}
+
+/*Sample output-
+****
+ ***
+  **
+   *
+*/
