df150b083b24c1f53cadd063574f3049937d6cef
Divyam Verma <60467333+pokeychip19946@users.noreply.github.com>
2022-01-12 23:59:49 +0530

    Update Pattern1.cpp

--- Patterns/Pattern1.cpp
+++ Patterns/Pattern1.cpp
@@ -1,1 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifndef ONLINE_JUDGE
+#define freopen freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+#else
+#define freopen 
+#endif
 
+int main()
+{
+  freopen;
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++)
+    {
+      for(int j=1; j<=n; j++)
+      {
+        if(j<=i)
+        {
+          for(int k=1; k<=n; k++)
+          {
+            if(k<j) cout<<"*";
+          }
+
+          cout<<j;
+        }
+      }
+      cout<<"\n";
+    }
+  }
+}
+
+
+/*sample output - 
+1
+1*2
+1*2**3
+1*2**3***4
+*/
