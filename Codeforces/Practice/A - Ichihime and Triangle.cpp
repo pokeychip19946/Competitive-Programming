e46bfb6179eff4a2ce7ef98817c10c3ad5f888f7
Divyam Verma <60467333+pokeychip19946@users.noreply.github.com>
2022-02-17 20:16:54 +0530

    Create A - Ichihime and Triangle.cpp

---
+++ Codeforces/Practice/A - Ichihime and Triangle.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifndef ONLINE_JUDGE
+#define freopen freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+#else
+#define freopen 
+#endif
+
+int main()
+{
+  freopen;
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    int a, b, c, d;
+    cin>>a>>b>>c>>d;
+    cout<<b<<" "<<c<<" "<<c<<endl;
+  }
+
+  return 0;
+}
