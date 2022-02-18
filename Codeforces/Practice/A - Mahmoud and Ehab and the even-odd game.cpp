580a0fc2aaeefd7521593657e6f7383c0e91ae1c
Divyam Verma <60467333+pokeychip19946@users.noreply.github.com>
2022-02-18 21:30:43 +0530

    Create A - Mahmoud and Ehab and the even-odd game.cpp

---
+++ Codeforces/Practice/A - Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifndef ONLINE_JUDGE
+#define freopen freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+#else
+#define freopen 
+#endif
+
+int main()
+{
+  freopen;
+  int t;
+  cin>>t;
+  if(t%2==0) 
+    cout<<"Mahmoud"<<endl;
+  else 
+    cout<<"Ehab"<<endl;
+
+  return 0;
+}
