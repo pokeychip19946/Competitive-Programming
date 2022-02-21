8bb7e5cb20e8225522c2a5da959e4403e14cfed0
Divyam Verma <60467333+pokeychip19946@users.noreply.github.com>
2022-02-21 22:35:20 +0530

    Create BOTTOM.cpp

---
+++ Codechef/COOK138D (Div-4)/BOTTOM.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifndef ONLINE_JUDGE
+#define freopen freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+#else
+#define freopen 
+#endif
+
+int main()
+{
+  freopen;
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    int n;
+    cin>>n;
+    int a = 7-n;
+    cout<<a<<endl;
+  }
+
+  return 0;
+}
