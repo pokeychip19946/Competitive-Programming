347292b979f74a95ee05d7620ba1b525691aea53
Divyam Verma <60467333+pokeychip19946@users.noreply.github.com>
2022-03-01 22:08:50 +0530

    Create MOVIE2X.cpp

---
+++ Codechef/LTIME105C (Div-3)/MOVIE2X.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int t=0;
+  // cin>>t;
+  int x, y;
+  cin>>x>>y;
+  int c=x-y;
+  int a=y/2;
+  int b=a+c;
+  cout<<b<<endl;
+
+  return 0;
+}
