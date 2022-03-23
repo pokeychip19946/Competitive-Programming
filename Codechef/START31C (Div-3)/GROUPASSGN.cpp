1faa9ac1f0548b63e8436e444f1831ef605e20ab
Divyam Verma <60467333+pokeychip19946@users.noreply.github.com>
2022-03-23 21:43:40 +0530

    Create GROUPASSGN.cpp

---
+++ Codechef/START31C (Div-3)/GROUPASSGN.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    int n,x;
+    cin>>n>>x;
+    int a=2*n;
+    int b=a-(x-1);
+    cout<<b<<endl;
+  }
+
+  return 0;
+}
